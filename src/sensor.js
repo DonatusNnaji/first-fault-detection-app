397c7fe4ed995e5b14d157ea06804838c0afb26a
donatus <natus2004@yahoo.com>
2023-06-09 17:55:27 +0000

    Resolved indicator logic issue

--- src/sensor.js
+++ src/sensor.js
@@ -10,6 +10,7 @@ function Sensor () {
     const {name, value} = event.target;
     const upDatesensor = (prevValue) => ({ ...prevValue, [name]: value }) ;
     setSensorValues(upDatesensor);
+    //indicatorChange([name]);
   }
 
   function sdgSign (minThreshold, val, maxThreshold) {
@@ -53,7 +54,23 @@ function Sensor () {
    }
    if (!ok){document.getElementById('faultMessages').innerHTML = " No fault detected!" ;}
 
+
+
 };
+//function indicatorChange(sliderId) {
+  //const indicatorId = `indicator${sliderId.slice(-1)}`;
+//  const indicator = document.getElementById(indicatorId);
+//  const colorValue = sensorvalues[`sensor${sliderId.slice(-1)}`]; // Assuming sensorvalues is an array or object
+//  indicator.style.backgroundColor = (colorValue < 50) ? 'yellow' : ((colorValue >= 50 && colorValue <= 150)) ? 'green' : 'red';
+//}
+
+//function indicatorChange(sliderId) {
+  //const indicatorId = `indicator${sliderId.slice(-1)}` ;
+  //const indicator = document.getElementById(indicatorId);
+  //const colorValue = `sensorvalues.sensor${sliderId.slice(-1)}`;
+//  indicator.style.backgroudColor = (colorValue < 50)? 'yellow'
+  //: ((colorValue >= 50) && (colorValue <= 150))? 'green' : 'red' ;
+//}
 
   const handleReset = (event) => {
      setSensorValues({ sensor1: 100, sensor2: 100, sensor3: 100, sensor4: 100, sensor5: 100, sensor6: 100});
@@ -61,7 +78,7 @@ function Sensor () {
   }
   return (
     <div  className= 'container con-box card'>
-     <h1>steam boiler fault detection and diagnosis panel</h1>
+     <h1>steam boiler fault detection and diagnosis simulation panel</h1>
 
      <form className= ' form-wrapper d-flex' >
 
@@ -70,8 +87,8 @@ function Sensor () {
         <input type= 'range' name= 'sensor1' id= 'slider1' mim= {0} max= {200} value= {sensorvalues.sensor1} onChange= {handleSensorChange} />
         <div className= 'outcome'>
           <span className= 'badge bg-secondary' id= 'reading1'>{sensorvalues.sensor1} </span>
-          <span  className= 'indicator ' id= 'indicator1' /*style= {`{backgroudColor: ( ${ sensorvalues.sensor1} < 100)? 'yellow':
-           ( ${ sensorvalues.sensor1} == 100) ? 'green': 'red' ;}`}*/> </span>
+          <span  className= 'indicator ' id= 'indicator1' style= {{backgroundColor: (sensorvalues.sensor1 < 50)? 'yellow':
+           ((sensorvalues.sensor1 >= 50 ) && (sensorvalues.sensor1 <= 150)) ? 'green': 'red' }  } > </span>
         </div>
       </div>
 
@@ -80,8 +97,8 @@ function Sensor () {
         <input type= 'range' name= 'sensor2' id= 'slider2' min= {0} max= {200} value= {sensorvalues.sensor2} onChange= {handleSensorChange} />
         <div className= 'outcome'>
           <span className= 'badge bg-secondary' id= 'reading2'>{sensorvalues.sensor2} </span>
-          <span  className= 'indicator' id= 'indicator2' /*style= {`{backgroudColor: ( ${ sensorvalues.sensor2}< 100)? 'yellow':
-           ( ${ sensorvalues.sensor2} == 100)? 'green': 'red' ;}`}*/>  </span>
+          <span  className= 'indicator' id= 'indicator2' style= {{backgroundColor: (sensorvalues.sensor2 < 50)? 'yellow':
+           ((sensorvalues.sensor2 >= 50 ) && (sensorvalues.sensor2 <= 150)) ? 'green': 'red' }}>  </span>
         </div>
       </div>
 
@@ -90,8 +107,8 @@ function Sensor () {
         <input type= 'range' name= 'sensor3' id= 'slider3' mim= {0} max= {200} value= {sensorvalues.sensor3} onChange= {handleSensorChange} />
         <div className= 'outcome'>
           <span className= 'badge bg-secondary' id= 'reading3'>{sensorvalues.sensor3} </span>
-          <span  className= 'indicator' id= 'indicator3'/* style= {`{backgroudColor: ( ${ sensorvalues.sensor3} < 100)? 'yellow':
-           ( ${ sensorvalues.sensor3} == 100) ? 'green': 'red' ;}`}*/>  </span>
+          <span  className= 'indicator' id= 'indicator3' style= {{backgroundColor: (sensorvalues.sensor3 < 50)? 'yellow':
+           ((sensorvalues.sensor3 >= 50 ) && (sensorvalues.sensor3 <= 150)) ? 'green': 'red' }}>  </span>
         </div>
       </div>
 
@@ -100,8 +117,8 @@ function Sensor () {
         <input type= 'range' name= 'sensor4' id= 'slider4' mim= {0} max= {200} value= {sensorvalues.sensor4} onChange= {handleSensorChange} />
         <div className= 'outcome'>
           <span className= 'badge bg-secondary' id= 'reading4'>{sensorvalues.sensor4} </span>
-          <span  className= 'indicator' id= 'indicator4' /*style= {`{backgroudColor: ( ${ sensorvalues.sensor4} < 100)? 'yellow':
-           ( ${ sensorvalues.sensor4} == 100) ? 'green': 'red' ;}`}*/>   </span>
+          <span  className= 'indicator' id= 'indicator4' style= {{backgroundColor: (sensorvalues.sensor4 < 50)? 'yellow':
+           ((sensorvalues.sensor4 >= 50 ) && (sensorvalues.sensor4 <= 150)) ? 'green': 'red' }}>   </span>
         </div>
       </div>
 
@@ -110,8 +127,8 @@ function Sensor () {
         <input type= 'range' name= 'sensor5' id= 'slider5' mim= {0} max= {200} value= {sensorvalues.sensor5} onChange= {handleSensorChange} />
         <div className= 'outcome'>
           <span className= 'badge bg-secondary' id= 'reading5'>{sensorvalues.sensor5} </span>
-          <span  className= 'indicator' id= 'indicator5' /*style= {`{backgroudColor: ( ${ sensorvalues.sensor5} < 100)? 'yellow':
-           ( ${ sensorvalues.sensor5} == 100) ? 'green': 'red' ;}`}*/> </span>
+          <span  className= 'indicator' id= 'indicator5' style= {{backgroundColor: (sensorvalues.sensor5 < 50)? 'yellow':
+           ((sensorvalues.sensor5 >= 50 ) && (sensorvalues.sensor5 <= 150)) ? 'green': 'red' }}> </span>
         </div>
       </div>
 
@@ -120,12 +137,12 @@ function Sensor () {
         <input type= 'range' name= 'sensor6' id= 'slider6' mim= {0} max= {200} value= {sensorvalues.sensor6} onChange= {handleSensorChange} />
         <div className= 'outcome'>
           <span className= 'badge bg-secondary' id= 'reading6'>{sensorvalues.sensor6} </span>
-          <span  className= ' indicator' id= 'indicator6'/* style= {`{backgroudColor: ( ${ sensorvalues.sensor6} < 100)? 'yellow':
-           ( ${ sensorvalues.sensor6} == 100) ? 'green': 'red' ;}`}*/>   </span>
+          <span  className= ' indicator' id= 'indicator6' style= {{backgroundColor: (sensorvalues.sensor6 < 50)? 'yellow':
+           ((sensorvalues.sensor6 >= 50 ) && (sensorvalues.sensor6 <= 150)) ? 'green': 'red' }}>   </span>
         </div>
       </div>
       <div className= 'button-con' >
-        <button className= 'btn btn-secondary' type= "reset" name="button1" onClick ={handleReset}>Reset Slider</button>
+        <button className= 'btn btn-secondary' type= "reset" name="button1" onClick ={handleReset}>Reset Sensor</button>
         <button className= 'btn btn-danger' type= "submit" name="button2" onClick={handleSumit}>Run Test </button>
       </div>
 
